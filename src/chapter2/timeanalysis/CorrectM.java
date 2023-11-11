eb1c12e1b2a42ae773923450229a7201b36d192a
amirparvaneh <amirparvaneh138@gmail.com>
2023-11-11 16:06:07 +0330

    welcome to algorithm

---
+++ src/chapter2/timeanalysis/CorrectM.java
@@ -0,0 +1,34 @@
+package chapter2.timeanalysis;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class CorrectM {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String s = scanner.nextLine();
+        String[] s1 = s.split(" ");
+        List<Long> sequence = Arrays.stream(s1).map(Long::parseLong).sorted().collect(Collectors.toList());
+
+        int mid;
+        if (n % 2 == 0) {
+            mid = (((n - 1) / 2));
+        } else {
+            mid = (((n - 1) + 1) / 2);
+        }
+
+        Long M = sequence.get(mid);
+        Long sum = 0L;
+        for (int i = 0; i < n; i++) {
+            sum += Math.abs(sequence.get(i) - M);
+        }
+
+        System.out.println(M + " " + sum);
+
+    }
+}
